463d67d0425327f41e0c74e41dcd2472415d9018
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2024-07-25 15:29:11 -0300

    Configure Nest.js API

--- packages/core/src/agendamento/Agendamento.ts
+++ packages/core/src/agendamento/Agendamento.ts
@@ -1,10 +1,10 @@
-import { Profissional } from '../profissional'
-import { Servico } from '../servico'
+import { Profissional } from "../profissional";
+import { Servico } from "../servico";
 
 export default interface Agendamento {
-    id: number
-    emailCliente: string
-    data: Date
-    profissional: Profissional
-    servicos: Servico[]
+  id: number;
+  emailCliente: string;
+  data: Date;
+  profissional: Profissional;
+  servicos: Servico[];
 }
